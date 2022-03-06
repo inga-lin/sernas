b6f064d62cc56128fe651337eb870d046e9baf10
Inga <inga.lingiene12@gmail.com>
2022-03-06 20:00:52 +0200

    mmm

--- nd/masyvas3/app.js
+++ nd/masyvas3/app.js
@@ -74,3 +74,80 @@ for (i = 0; i < bitGirls.length; i++){
 } 
 
 console.log(happyCats);
+
+console.log('--------');
+
+/*masyvo N.D.*/
+//1Sukurti masyvą (piniginę), kurio ilgis yra atsitiktinis nuo 10 iki 30, o reikšmės atsitiktiniai skaičiai nuo 0 iki 10 (pinigai);
+const pinigine = [];
+for (i = 0; i < rand(10, 30); i++ ){
+    pinigine.push(rand(0, 10));//push tik masyve
+}
+
+console.log(pinigine);
+
+//2Naudojant ciklą apskaičiuoti masyvo iš 1 uždavinio reikšmių (pinigų esančių piniginėje) sumą;
+
+let piniguSuma = 0;
+for ( i = 0; i < pinigine.length; i++){
+    piniguSuma = piniguSuma + pinigine[i];
+}
+
+console.log(piniguSuma);
+
+//3Naudojant ciklą apskaičiuoti masyvo iš 1 uždavinio popierinių pinigų (skaičių didesnių už 2 ) reikšmių sumą;
+
+
+let sumaPinigu = 0;
+
+for ( i = 0; i < pinigine.length; i++){
+    if (pinigine[i] > 2){
+        sumaPinigu = sumaPinigu + pinigine[i]
+    }
+}
+console.log(sumaPinigu);
+
+//4 Išleisti visus metalinius pinigus (reikšmes, kurios yra mažesnės arba lygios 2 padaryti lygias 0) iš 1 uždavinio;
+const naujaPinigine = [];
+
+for (i = 0; i < pinigine.length; i++){
+    if (pinigine[i]  > 2){
+        naujaPinigine.push(pinigine[i]);
+    } else {
+        naujaPinigine.push(0);
+    }
+}
+    
+console.log(naujaPinigine);
+
+//5.Surasti didžiausią reikšmę 1 uždavinio masyve ir paskaičiuoti kiek tokių didžiausių reikšmių masyve yra;
+
+let didziausiasSkaicius = 0;
+
+for ( i = 0; i < pinigine.length; i++){
+    if(pinigine[i] > didziausiasSkaicius){
+        didziausiasSkaicius = pinigine[i];
+    }
+}
+
+console.log(didziausiasSkaicius);
+
+let didziausiuSkaiciuSkaicius = 0;
+for ( i = 0; i < pinigine.length; i++){
+    if (didziausiasSkaicius === pinigine[i]){
+        didziausiuSkaiciuSkaicius += 1;
+    }
+}
+
+console.log(didziausiuSkaiciuSkaicius);
+
+
+//6.Visus masyvo elementus, kurie yra lygūs 0, pakeisti į tų elementų indeksų (vietų, numerių) reikšmes;
+
+const naujasMasyvas = [];
+for (i = 0; i < pinigine.length; i++){
+    if(pinigine[i] === 0){
+        naujasMasyvas.push()
+    }
+}
+console.log(naujasMasyvas);
