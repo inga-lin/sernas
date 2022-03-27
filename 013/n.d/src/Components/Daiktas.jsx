180873236e560636835c67fa1e2280511cf7359f
Inga <inga.lingiene12@gmail.com>
2022-03-27 21:05:08 +0300

    n.d

--- 013/n.d/src/Components/Daiktas.jsx
+++ 013/n.d/src/Components/Daiktas.jsx
@@ -1,12 +1,36 @@
-function rand(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+
 
 function Daiktas({auto}) {//auto=vardas
+    if(auto.id % 2 !== 0){ 
+        return (
+            <div>
+                <h3 style={{color: auto.color}}>{auto.id} {auto.type} {auto.name} neporinis id</h3>
+            </div>
+        )
+    }else {
+        return(
+            <div>
+                <h3 style={{color: auto.color}}>{auto.id} {auto.type} {auto.name} porinis id</h3>
+            </div> 
+        )
+    }
+    
+    
+}
+      
+export default Daiktas;
+
+/*    }else {
+        return(
+            <h3 style={{color:vardas.color}}> {vardas.type} surudijusi gelda</h3>
+        )
+    }*/
+
+
+
+/*
    // if(vardas.id !== vardas.id[i] % 2){ //if(vardas.type !== 'MB') jeigu tai ne MB tai spauzdink tai ka reik o jeigu tai yra MB spauzdink surudijusi gelda
-   
+   // for (let i = 0; i < auto.lenth; i++){ 
         if(auto.id % 2 !== 0){ 
           return (
             <div>
@@ -22,13 +46,4 @@ function Daiktas({auto}) {//auto=vardas
                 <h3 style={{color:auto.color}}> {auto.type} porinis id</h3>
             )
         }
-   
-}
-      
-export default Daiktas;
-
-/*    }else {
-        return(
-            <h3 style={{color:vardas.color}}> {vardas.type} surudijusi gelda</h3>
-        )
-    }*/
+*/
--- 013/n.d/src/Components/Tvenkinys.jsx
+++ 013/n.d/src/Components/Tvenkinys.jsx
@@ -1,21 +1,57 @@
+
 import Daiktas from './Daiktas';
 //import { v4 as uuidv4 } from 'uuid'; //autos.map((auto, i) => <Automobilis key={uuidv4()} vardas={auto}></Automobilis>)
 ////cia galima sukurti unikalu key numeri 
 //1-terminale paraso, npm install uuid 
 //2- antroje eiluteje irasom import { v4 as uuidv4 } from 'uuid';
 // 3-11eiluteje yrasytume autos.map((auto, i) => <Automobilis key={uuidv4()} vardas={auto}></Automobilis>)
-function Tvenkinys({auto}) {
-    return (
-        <div className="aa">  {/*taip padarom lista*/}
-               
-            <Daiktas auto={auto}></Daiktas>
-            <Daiktas auto={auto}></Daiktas>
+/*function Tvenkinys({auto}) {
+    const skaicius =  Math.random() < 0.5;
+    if (skaicius){
+        for (let i = 0; i < auto.length; i++){
+            if(auto.id % 2 === 0 ){
+                return (
+                    <Daiktas auto={auto}></Daiktas>
             
-            
-       
-        </div>   
-
-    );
+                ) 
+            }
+        }
+    } else {
+        for (let i = 0; i < auto.length; i++){
+            if(auto.id % 2 !== 0 ){
+                return(
+                    <Daiktas auto={auto}></Daiktas>
+                )
+            }
+        }
+    }
 }
   
-  export default Tvenkinys;
+export default Tvenkinys;*/
+
+
+  
+/*export default function Tvenkinys({auto}) {
+    if (auto.id % 2 !== 0 ) {
+        return (
+            <Daiktas auto={auto}></Daiktas>
+        )
+    } else if (auto.id % 2 === 0 ){
+        return (
+            null
+        )
+    } 
+    
+}*/
+
+export default function Tvenkinys({auto}) {
+    return (
+        <div>
+            <Daiktas auto={auto}></Daiktas>
+            <Daiktas auto={auto}></Daiktas>
+        </div>
+
+    )
+ 
+    
+}
