a71e60752d620d549afa482aa3bf9b549e1424ef
Inga <inga.lingiene12@gmail.com>
2022-03-29 16:06:17 +0300

    n

--- 013/r1/src/App.jsx
+++ 013/r1/src/App.jsx
@@ -35,3 +35,20 @@ function App() {
 }
 
 export default App;*/
+
+/*
+REACT BASE
+
+
+
+Sukurti komponentą, kuris užrašytų “LABAS, ZUIKI!”. Raidžių spalva pink. Spalva tekstui nurodoma komponento viduje naudojant style.
+
+Sukurti komponentą, kuris gauną vieną props. Props yra bet koks tekstas, kuris komponente atvaizduojamas h1 tage.
+
+Sukurti komponentą rodantį tekstą h1 tage- “Zebrai ir Bebrai”, kuris gauna vieną props, kuris lygus 1 arba 2. Jeigu props lygus 1 tekstas nudažomas mėlynai, o jeigu 2 - raudonai.
+
+Sukurti komponentą, kuris gauna du props. Vienas props bet koks tekstas, kuris komponente atvaizduojamas h1 tage, o antras bet koks tekstas kuris atvaizduojamas h2 tage.
+
+Sukurti komponentą, kuris gauna tris props. Vienas props bet koks tekstas, kuris komponente atvaizduojamas h1 tage, o antras bet koks tekstas kuris atvaizduojamas h2 tage, o trečias yra spalva, kuria nudažomi abu tekstai.
+
+*/
