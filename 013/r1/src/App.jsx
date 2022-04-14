1215dd87e0c1e37ac50f495db8781687dc19cf19
Inga <inga.lingiene12@gmail.com>
2022-04-14 10:10:00 +0300

    n

--- 013/r1/src/App.jsx
+++ 013/r1/src/App.jsx
@@ -108,3 +108,12 @@ function App() {
 
 
 export default App;
+
+/*
+Mini localStorage CRUD
+
+
+Ganykla. Turim trijų rūšių gyvulius: avis, antis ir antilopes. Kiekvienas gyvulys turi savo svorį.
+Parašyti localStorage CRUD aplikaciją, kurioje būtų galima pridėti naujus gyvulius su jų svoriais į ganyklą, ištrinti iš ganyklos ir redaguoti kiekvieno jų svorį.
+
+*/
